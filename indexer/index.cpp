2fbc546cb1e7099e51aa971a5f98bcc20e53ddee
Nikita Ofitserov <himikof@gmail.com>
2013-05-07 00:28:48 +0400

    Synchronize index access

--- indexer/index.cpp
+++ indexer/index.cpp
@@ -4,6 +4,9 @@
 #include <boost/format.hpp>
 #include <boost/range/algorithm.hpp>
 #include <boost/range/algorithm_ext.hpp>
+#include <boost/thread/shared_mutex.hpp>
+#include <boost/thread/locks.hpp>
+#include <boost/thread/lock_types.hpp>
 
 #include "trie.hpp"
 #include "exceptions.hpp"
@@ -21,6 +24,8 @@ struct pimpl<indexer::index>::implementation
 
     trie forward;
     trie reverse;
+
+    boost::shared_mutex mutex;
 };
 
 namespace indexer {
@@ -33,6 +38,7 @@ index::index(fs::path const& path)
 void index::insert(boost::string_ref const& data)
 {
     implementation& impl = **this;
+    boost::unique_lock<boost::shared_mutex> lock(impl.mutex);
     std::string s(data);
     s += EOS;
     impl.forward.insert(s);
@@ -43,6 +49,7 @@ void index::insert(boost::string_ref const& data)
 void index::search(boost::string_ref const& data, size_t k, bool has_transp, results_t& results)
 {
     implementation& impl = **this;
+    boost::shared_lock<boost::shared_mutex> lock(impl.mutex);
     if (k != 0) {
         size_t switch_len = data.size() / 2;
         size_t switch_len_1 = data.size() - switch_len;
