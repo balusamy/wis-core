e4f80b9783c3a66eaedfddf42a711d0ae86072f7
Kirill Elagin <kirelagin@gmail.com>
2013-05-06 20:25:01 +0400

    Better stop-words filtering

--- python/nlp.py
+++ python/nlp.py
@@ -12,7 +12,7 @@ def has_char(w):
     return False
 
 def is_good_word(w):
-    return len(w) > 2 and w not in stop and has_char(w)
+    return len(w) > 2 and has_char(w)
 
 def tokenise(text):
     return [w for s in sent_tokenize(text) for w in word_tokenize(s)]
@@ -20,11 +20,15 @@ def tokenise(text):
 def normalise(words):
     words = enumerate(words)
     words = filter(lambda p: is_good_word(p[1]), words)
+    words = [(i, w.lower()) for i, w in words]
+    words = filter(lambda p: p[1] not in stop, words)
     words = [(i, snowball.stem(w)) for i, w in words]
     return words
 
 def normalise_drop(words):
     words = filter(is_good_word, words)
+    words = map(lambda w: w.lower(), words)
+    words = filter(lambda w: w not in stop, words)
     words = map(snowball.stem, words)
     return words
 
